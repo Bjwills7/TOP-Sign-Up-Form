3993b5d523f466defe44ed7375027e0d11b27008
Bjwills7 <bjwills7@hotmail.com>
2023-02-23 05:29:56 -0600

    Fix password validation

--- script.js
+++ script.js
@@ -2,12 +2,22 @@ const pass = document.querySelector('#password');
 const passConfirm = document.querySelector('#confirmPassword');
 const button = document.querySelectorAll('button');
 
+passConfirm.addEventListener('change', () => {resetValidity()});
+pass.addEventListener('change', () => {resetValidity()});
+
 function checkPass() {
     if (pass.value === passConfirm.value) {
         return true;
     } else {
         pass.classList.add('error');
         passConfirm.classList.add('error');
+        pass.setCustomValidity('Passwords do not match');
+        pass.reportValidity();
         return false;
     }
 } 
+
+function resetValidity() {
+    pass.setCustomValidity('');
+    pass.reportValidity();
+}
